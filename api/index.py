d32f9342769a55f01b5579340fa519a72b8eea13
DEVIL <83532735+DEVIL-X-0P@users.noreply.github.com>
2025-05-10 14:45:59 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,7 +8,7 @@ from urllib.parse import parse_qs, urlparse
 
 app = Flask(__name__)
 
-
+#UNWORKING COOKIES JUST FOR DEMO
 # cookies = {
 #     'ndut_fmt': 'C96770F2F5849984C2BA2A3F6284E3769F4147D175CD113DA30A8F2DE4CCD1FF',
 #     'ndus': 'YvdD63MteHuiw0IgOC3Kdp-b5nahyIkzD_XdJDj2',
@@ -21,16 +21,38 @@ app = Flask(__name__)
 #     'PANWEB': '1'
 # }
 
+
+#WORKING COOKIES
+# cookies = {
+#     'PANWEB': '1',
+#     'browserid': 'p4nVrnlkUVKcnbbJHnIClAhSL5uXs01e-0svx0bm7KHLUB6wIVvCUNGLIpU=',
+#     'lang': 'en',
+#     '__bid_n': '1900b9f02442253dfe4207',
+#     'ndut_fmt': '5E7E5AFA065E159EF56CFE164FCF084C72B603BE3611911C28550443BDC08A4B',
+#     '__stripe_mid': 'b85d61d2-4812-4eeb-8e41-b1efb3fa2a002a54d5',
+#     'ndus': 'YylKpiCteHuiYEqq8n75Tb-JhCqmg0g4YMH03MYD',
+#     'csrfToken': 'zAVdnQAVegC92-ah6pmLf6Dl',
+# }
+
+
 cookies = {
-    'PANWEB': '1',
-    'browserid': 'p4nVrnlkUVKcnbbJHnIClAhSL5uXs01e-0svx0bm7KHLUB6wIVvCUNGLIpU=',
     'lang': 'en',
-    '__bid_n': '1900b9f02442253dfe4207',
-    'ndut_fmt': '5E7E5AFA065E159EF56CFE164FCF084C72B603BE3611911C28550443BDC08A4B',
-    '__stripe_mid': 'b85d61d2-4812-4eeb-8e41-b1efb3fa2a002a54d5',
-    'ndus': 'YylKpiCteHuiYEqq8n75Tb-JhCqmg0g4YMH03MYD',
-    'csrfToken': 'zAVdnQAVegC92-ah6pmLf6Dl',
+    '__stripe_sid': '6ba5495f-e831-4a9c-a513-7a7ab2b7e0b9d5eb6f',
+    '_ga_K6JMPYL99R': 'GS2.1.s1746852518$o1$g1$t1746856379$j0$l0$h0',
+    'ab_ymg_result': '{"data":"da8926eb8716ff12c51c9bf40086f24127c2e3126b3a8c66530a42d50daa40797711e2873fe863d994f415fc0335acaddfb3aaeae3cc0a05231c5ffff2168e9b6f0aadbd833774602694a2e0fb899e27ca7e5aa108da809a42d639020c86fc4022ab1219e235d6c8b6819e3121cdb68975dec4ba636c79cbf05dcd8277382169","key_id":"66","sign":"e727e169"}',
+    '_ga_06ZNKL8C2E': 'GS2.1.s1746858983$o2$g1$t1746858997$j46$l0$h0',
+    '_gcl_au': '1.1.432005496.1746852519',
+    'ndus': 'YdZTyX1peHuimlux_D6dLGQBeHmj0r3M3trkunHB',
+    '_ga': 'GA1.1.1786971687.1746852519',
+    'ab_sr': '1.0.1_N2I5MDI2MWFhMmMxOGE1MWU5ZTRlZjJhYzQwM2M0NmI4ZTZjNmZhZTQ5Nzk2MzAxNTA4ZmE3OTkyZTVhZGYyMzYyZTBlOWQ1NDUxMmE4YWQ3MWJhN2VhMzc4ZTI5YmFjZDA0ODBmNTRiMDU4MzRmOWZiYjc0ZjRiMGNlYzRmYTQ4MTYzZGI4Mzc5YzdhZDUyYWFkZjIxZTRiMTBkZTk5Zg==',
+    '__bid_n': '196b885ed7339790814207',
+    '__stripe_mid': '31fc92f3-a12a-480f-9e44-53f30f08258a75588e',
+    'browserid': '45Cbkepkx0J0bqgQi1e1ubtbstmebahkYOYm3ZWuIktZFHaUuRjvdeeHz24=',
+    'PANWEB': '1',
+    'csrfToken': 'C84Gc54sleTMoZxBx24k4SM7',
+    'ndut_fmt': '26BE3C564003BEB97F4ED69DEBD9974F6291C708F2B2B419F0B6282675131E1C',
 }
+
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0',
